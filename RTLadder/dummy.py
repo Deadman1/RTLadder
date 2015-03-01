4092eb335b642fd38e76754bb473970fa517846c
Deadman <warlighter1@gmail.com>
2015-02-28 19:27:33 -0800

    - Added javascript for Google Analytics reports - Added all template links on ladder page

--- RTLadder/dummy.py
+++ RTLadder/dummy.py
@@ -1,24 +1,10 @@
-class game:
-    a=1
-    b=2
-    c=3
-    
-    
-print "hi"    
+players = ['master of desaster', 'Jefferspin']
 
-gameList = []
-for i in range(1,5):
-    gameList.append(game())
-    
-print gameList    
-for i in range(1,5):
-    print i, gameList[i-1].a
-    
-    
-for i in range(1,5):
-    gameList[i-1].a = 4
 
-print "--------------------------"
-for i in range(1,5):
-    print i, gameList[i-1].a
+gameName = 'Deadman\'s ladder : ' +  ' vs '.join([p for p in players])
 
+print gameName
+
+gameName = gameName[:40] + "..."
+
+print "--" + gameName
